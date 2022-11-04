b476dfe9ea7e005cd879095f14316178fc9ee129
Sebastian <smcasanova17@hotmail.com>
2022-11-03 22:03:07 -0400

    created table elements and added bulma classes, make git buynance function that makes table function

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,30 @@
-var userInput = 
-$.get("https://api2.binance.com/api/v3/ticker/24hr?symbol=",function
-(data){
-    console.log('data received:', data)
-});
+var tableBodyEl = document.querySelector('tbody')
+function getBinanceDaily() {
+    fetch("https://api2.binance.com/api/v3/ticker/24hr").then((response)=>{
+        if(!response.ok){
+            console.log(response)
+            document.write('response is not ok')
+        }
+        return response.json()
+}).then((data) => {
+        console.log(data[0])
+        for(var i=0;i<10;i++){
+            // creates table row element that defines a row of cells in a table.
+            var coinTr = document.createElement('tr')
+            // creates tabele head element
+            var coinTh = document.createElement('th')
+            // adds the row attribute too cointh which defines the cell
+            coinTh.scope = "row"
+            
+            var cointd = document.createElement('td')
+           // cointd.textContent = data
+            coinTh.textContent = data[i].symbol
+            coinTr.appendChild(cointd)
+            coinTr.appendChild(coinTh)
+            //cointd.innerText=data[i].symbol
+            tableBodyEl.appendChild(coinTr)
+        }
+        
+    })
+}
+getBinanceDaily()
